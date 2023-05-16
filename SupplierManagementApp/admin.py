6a9161bd5c1dcdeea721d200766219f00819ee42
Chan-Paul-Amol <99714187+Chan-Paul-Amol@users.noreply.github.com>
2023-05-16 20:57:17 +0700

    Admin Register Models

--- SupplierManagementApp/admin.py
+++ SupplierManagementApp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Supplier, AHPcalculation, Criteria, Weights, BenefitCost
 
-# Register your models here.
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'email', 'phone_number')
+
+
+class AHPcalculationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'supplier', 'results', 'ranking')
+
+
+class CriteriaAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price',
+                    'delivery_time', 'cost', 'distance')
+
+
+class WeightsAdmin(admin.ModelAdmin):
+    list_display = ('criteria_id', 'value')
+
+
+class BenefitCostAdmin(admin.ModelAdmin):
+    list_display = ('criteria_id', 'benefit', 'cost')
+
+
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(AHPcalculation, AHPcalculationAdmin)
+admin.site.register(Criteria, CriteriaAdmin)
+admin.site.register(Weights, WeightsAdmin)
+admin.site.register(BenefitCost, BenefitCostAdmin)
